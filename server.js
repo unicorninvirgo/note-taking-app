57ec2423738d3b83267f9647e14972ff4119443f
Unicorn Virgo <58121705+unicorninvirgo@users.noreply.github.com>
2021-10-11 18:04:06 -0400

    Moved index file to parent path to display in github

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 );
 
 // GET Route for feedback page
